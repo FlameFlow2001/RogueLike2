d6d5864b03da1b87d10aa9f291885d2033ce4b07
FlameFlow2001 <work4ksh@gmail.com>
2022-02-19 21:02:49 +0300

    Fixed standart projectile destroying animation
    Standart projectile plays destroying animation after hitting an obstacle

--- Assets/Resources/Scripts/PlayerProjectileScript.cs
+++ Assets/Resources/Scripts/PlayerProjectileScript.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
-using UnityEngine.UI;
+using System.Collections;
+
 public class PlayerProjectileScript : MonoBehaviour
 {
     public float minDamage;
     public float maxDamage;
-    private float lifeTime = 3f;
+    private float lifeTime = 2f;
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        animator.speed = 4;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag != "Player" && collision.tag != "Pickable" && collision.tag != "Projectile")
@@ -14,9 +22,16 @@ public class PlayerProjectileScript : MonoBehaviour
                 float damage = Mathf.RoundToInt(Random.Range(minDamage, maxDamage));
                 collision.GetComponent<Enemy>().DealDamage(damage);
             }
-            //animator.Play("Projectile_Destroying");
-            Destroy(gameObject, lifeTime);
+            animator.Play("ProjectileDestroying");
+            Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length / animator.speed);
         }
-        Destroy(gameObject, lifeTime);
+
+        IEnumerator DestroyProjectile()
+        {
+            yield return new WaitForSeconds(lifeTime);
+            animator.Play("ProjectileDestroying");
+            Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length / animator.speed);
+        }
+        StartCoroutine(DestroyProjectile());
     }
 }
