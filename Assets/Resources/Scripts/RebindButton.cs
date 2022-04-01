82269764af3da99d9cd968105a1ddb0b49b20b60
FlameFlow2001 <work4ksh@gmail.com>
2022-04-01 11:26:08 +0300

    Made tiled map, added few new packs

--- Assets/Resources/Scripts/RebindButton.cs
+++ Assets/Resources/Scripts/RebindButton.cs
@@ -7,29 +7,34 @@ public class RebindButton : MonoBehaviour
 {
     [SerializeField] private InputActionReference inputActionRef;
     [SerializeField] private TMP_Text buttonText;
+
     private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
     private void Start()
     {
-        int bindingIndex = inputActionRef.action.GetBindingIndexForControl(inputActionRef.action.controls[0]);
-        buttonText.text = InputControlPath.ToHumanReadableString(inputActionRef.action.bindings[bindingIndex].effectivePath,
+        buttonText.text = InputControlPath.ToHumanReadableString(inputActionRef.action.bindings[1].effectivePath,
             InputControlPath.HumanReadableStringOptions.OmitDevice);
     }
+
     public void StartRebinding()
     {
         rebindingOperation = inputActionRef.action.PerformInteractiveRebinding()
                     .WithCancelingThrough("<Keyboard>/escape")
-                    .WithControlsExcluding("Mouse")
                     .OnMatchWaitForAnother(0.1f)
-                    .OnCancel(operation => FindObjectOfType<EventSystem>().SetSelectedGameObject(null))
+                    .OnCancel(operation => RebindCancel())
                     .OnComplete(operation => RebindComplete())
                     .Start();
     }
 
     private void RebindComplete()
     {
-        int bindingIndex = inputActionRef.action.GetBindingIndexForControl(inputActionRef.action.controls[0]);
-        buttonText.text = InputControlPath.ToHumanReadableString(inputActionRef.action.bindings[bindingIndex].effectivePath,
+        buttonText.text = InputControlPath.ToHumanReadableString(inputActionRef.action.bindings[1].effectivePath,
             InputControlPath.HumanReadableStringOptions.OmitDevice);
+        rebindingOperation.Dispose();
+        FindObjectOfType<EventSystem>().SetSelectedGameObject(null);
+    }
+
+    private void RebindCancel()
+    {
         rebindingOperation.Dispose();
         FindObjectOfType<EventSystem>().SetSelectedGameObject(null);
     }
